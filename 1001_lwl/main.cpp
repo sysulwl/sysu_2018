340d8600fb65e0f8dca616ebc0fa56ef812cdb40
liuwanli <547058465@qq.com>
2018-04-15 19:33:00 +0800

    LWL 做 1001 题

---
+++ 1001_lwl/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	int i = 1; 
+	int k = 0;
+	for(int i = 1; i < 100; ++i)
+	{
+		k += 2;
+		cout<<"Hello World!\n";
+		
+	}
+	return 0;
+}
